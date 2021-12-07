9e3f7a20aa0c2c926f1c332ac4e33c2f0b3bdc40
abinappu11 <95327148+abinappu11@users.noreply.github.com>
2021-12-07 18:57:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// Script by Aju
-// AJU 𝚂𝙸𝚁 | 𝙳𝙾𝙽'𝚃 𝙲𝙷𝙰𝙽𝙶𝙴 𝙲𝚁𝙴𝙳𝙸𝚃𝚂🙂
-// ©Aju Bot 
+// Script by Appu
+// Appu 𝚂𝙸𝚁 | 𝙳𝙾𝙽'𝚃 𝙲𝙷𝙰𝙽𝙶𝙴 𝙲𝚁𝙴𝙳𝙸𝚃𝚂🙂
+// ©Appu Bot 
 const {
   WAConnection: _WAConnection,
   MessageType,
@@ -133,9 +133,9 @@ nopref = false
 numbernye = '0'
 autoketik = false;
 prefixStatus = true;
-targetpc = "917994873071"; 
-owner = "917994873071"; 
-fakeyoi = "AJU"; 
+targetpc "918943383242"; 
+owner = "918943383242"; 
+fakeyoi = "APPU"; 
 HunterApi = "Ikyy69", 
 xchillds = 'XChillDs' 
 hardi = 'hardianto', 
